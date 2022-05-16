3c9148db087b2dc5cfd95e71763c53ce035b4e80
Safinxr <safinxr@gmail.com>
2022-05-17 02:46:02 +0600

    delete api added

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await stockCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
